3eed574e7f1ce404b777b0e388216c912c4efd7b
quote <quote@dust.io>
2017-04-22 14:29:14 -0500

    reworked render logic. draws a box going up

--- main.cpp
+++ main.cpp
@@ -1,108 +1,96 @@
 // sample from https://wiki.libsdl.org/SDL_RenderCopy
 #include "SDL2/SDL.h"
-#define SHAPE_SIZE 16
-
-bool init() {
-
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
-        return false;
-    }
-
-    return true;
-}
 
 int main(int argc, char *argv[])
 {
-  SDL_Window* Main_Window;
-  SDL_Renderer* Main_Renderer;
-  SDL_Surface* Loading_Surf;
-  SDL_Texture* Background_Tx;
-  SDL_Texture* BlueShapes;
 
-  /* Rectangles for drawing which will specify source (inside the texture)
-  and target (on the screen) for rendering our textures. */
-  SDL_Rect SrcR;
-  SDL_Rect DestR;
-
-  SrcR.x = 0;
-  SrcR.y = 0;
-  SrcR.w = SHAPE_SIZE;
-  SrcR.h = SHAPE_SIZE;
-
-  DestR.x = 640 / 2 - SHAPE_SIZE / 2;
-  DestR.y = 580 / 2 - SHAPE_SIZE / 2;
-  DestR.w = SHAPE_SIZE;
-  DestR.h = SHAPE_SIZE;
-
-
-  /* Before we can render anything, we need a window and a renderer */
-  Main_Window = SDL_CreateWindow("SDL_RenderCopy Example",
-  SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 580, 0);
-  Main_Renderer = SDL_CreateRenderer(Main_Window, -1, SDL_RENDERER_ACCELERATED);
-
-  /* The loading of the background texture. Since SDL_LoadBMP() returns
-  a surface, we convert it to a texture afterwards for fast accelerated
-  blitting. */
-  Loading_Surf = SDL_LoadBMP("Background.bmp");
-  Background_Tx = SDL_CreateTextureFromSurface(Main_Renderer, Loading_Surf);
-  SDL_FreeSurface(Loading_Surf); /* we got the texture now -> free surface */
-
-  /* Load an additional texture */
-  BlueShapes = SDL_CreateTextureFromSurface(Main_Renderer, Loading_Surf);
-  BlueShapes = SDL_CreateTexture(Main_Renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, SHAPE_SIZE, SHAPE_SIZE);
-
-  SDL_SetRenderTarget(Main_Renderer, BlueShapes);
-  SDL_SetRenderDrawColor(Main_Renderer, 0x00, 0x00, 0xff, 0xff);
-  SDL_RenderClear(Main_Renderer);
-
-  SDL_SetRenderTarget(Main_Renderer, NULL);
-
-  /* now onto the fun part.
-  This will render a rotating selection of the blue shapes
-  in the middle of the screen */
-  int i;
-  int n;
-  for (i = 0; i < 2; ++i) {
-    for(n = 0; n < 4; ++n) {
-      SrcR.x = SHAPE_SIZE * (n % 2);
-      if (n > 1) {
-        SrcR.y = SHAPE_SIZE;
-      } else {
-        SrcR.y = 0;
-      }
-
-      /* render background, whereas NULL for source and destination
-      rectangles just means "use the default" */
-      SDL_RenderCopy(Main_Renderer, Background_Tx, NULL, NULL);
-
-      /* render the current animation step of our shape */
-      SDL_RenderCopy(Main_Renderer, BlueShapes, &SrcR, &DestR);
-      SDL_RenderPresent(Main_Renderer);
-      SDL_Delay(500);
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
+        return 1;
     }
-  }
 
 
-  /* The renderer works pretty much like a big canvas:
-  when you RenderCopy() you are adding paint, each time adding it
-  on top.
-  You can change how it blends with the stuff that
-  the new data goes over.
-  When your 'picture' is complete, you show it
-  by using SDL_RenderPresent(). */
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Texture* background_map_tex;
+    SDL_Texture* player_tex;
+    SDL_Texture* merged_tex;
 
-  /* SDL 1.2 hint: If you're stuck on the whole renderer idea coming
-  from 1.2 surfaces and blitting, think of the renderer as your
-  main surface, and SDL_RenderCopy() as the blit function to that main
-  surface, with SDL_RenderPresent() as the old SDL_Flip() function.*/
+    // init window and renderer
+    window = SDL_CreateWindow("a small world", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 360, 240, 0);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-  SDL_DestroyTexture(BlueShapes);
-  SDL_DestroyTexture(Background_Tx);
-  SDL_DestroyRenderer(Main_Renderer);
-  SDL_DestroyWindow(Main_Window);
-  SDL_Quit();
+    // setup background
+    background_map_tex = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 720, 480);
+    SDL_SetRenderTarget(renderer, background_map_tex);
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x20, 0x88, 0xff);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderTarget(renderer, NULL);
 
+    // setup player - white square with black outline
+    const int player_height = 60;
+    const int player_width = 40;
 
-  return 0;
+    player_tex = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, player_height, player_width);
+    SDL_SetRenderTarget(renderer, player_tex);
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xff);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
+    SDL_RenderDrawRect(renderer, NULL); // draws a white outline
+    SDL_SetRenderTarget(renderer, NULL);
+
+    // this is where we draw everything to, which will eventually be scaled to the full screen
+    // we will work in 360x240 pixel range
+    merged_tex = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 360, 240);
+
+    SDL_Rect player_pos_rect;
+    player_pos_rect.h = player_height + 1; // dunno why i have to extend this by 1
+    player_pos_rect.w = player_width + 1;
+
+    player_pos_rect.y = 240 - player_height;
+    player_pos_rect.x = 130;
+
+    int anim_counter = 0;
+    const int anim_frames = 80;
+
+    SDL_Event event;
+    int t_start;
+    while(true) {
+        t_start = SDL_GetTicks();
+
+        SDL_PollEvent(&event);
+        if(event.type == SDL_QUIT)
+            break;
+
+        {
+            // render
+
+            // draw everything on merged_tex then copy to render buffer
+            SDL_SetRenderTarget(renderer, merged_tex);
+            SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+            SDL_RenderClear(renderer);
+
+            SDL_Rect player_anim_rect = player_pos_rect;
+
+            player_anim_rect.y -= (anim_counter % anim_frames); // 0 x 0 is top left
+            SDL_RenderCopy(renderer, background_map_tex, NULL, NULL);
+            SDL_RenderCopy(renderer, player_tex, NULL, &player_anim_rect);
+
+            SDL_SetRenderTarget(renderer, NULL);
+            SDL_RenderCopy(renderer, merged_tex, NULL, NULL);
+            SDL_RenderPresent(renderer);
+        }
+
+        SDL_Delay(16 - (SDL_GetTicks() - t_start));
+        anim_counter++;
+    }
+
+    SDL_DestroyTexture(background_map_tex);
+    SDL_DestroyTexture(player_tex);
+    SDL_DestroyTexture(merged_tex);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
